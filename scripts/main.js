7807a8fb7a753969b66a9a0ffed00dde4b2fe699
Maurici <hello@mauriciabad.com>
2020-03-14 23:32:39 +0100

    Fix realvh

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,7 @@
-window.addEventListener('resize', () => {
+function updateRealVH() {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+}
+
+updateRealVH();
+window.addEventListener('resize', updateRealVH);
