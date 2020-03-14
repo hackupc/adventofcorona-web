69fc9381832759ac9041bce06ce6866457f4cade
Maurici <hello@mauriciabad.com>
2020-03-15 00:51:20 +0100

    Share button

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,26 @@
+
+// Real 100vh
 function updateRealVH() {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
 }
-
 updateRealVH();
 window.addEventListener('resize', updateRealVH);
+
+
+// Share button
+const shareButton = document.getElementById('share');
+if (navigator.share) shareButton.parentElement.style.display = 'block';
+shareButton.addEventListener('click', event => {  
+  event.preventDefault();
+  if (navigator.share) {
+    navigator.share({
+      title: document.title,
+      url: window.location,
+      text: `Check this out. ${document.querySelector('meta[name="description"]').content}`,
+    })
+    .catch(console.error);
+  } else {
+    // fallback
+  }
+});
