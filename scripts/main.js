d22fefc29e2c8f219c23236945753c95302c3171
Maurici <hello@mauriciabad.com>
2020-03-15 02:03:37 +0100

    UI changes problems

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,25 @@
-
-// Real 100vh
+var currentProblemId = 0;
+var problems;
+var progress;
+Promise.all([
+  fetch('data/problems.json')
+    .then(response => response.json())
+    .then(content => problems = content),
+  fetch('data/progress.json')
+    .then(response => response.json())
+    .then(content => progress = content),
+]).then((values => {
+  router.navigateTo(`/problem/${currentProblemId}`);
+}));
+const problemTitleElem = document.getElementById('problem-title');
+const problemStatementElem = document.getElementById('problem-statement');
+const answerInputElem = document.getElementById('answer-imput');
+const feedbackElem = {
+  true: document.getElementById('feedback-true'),
+  false: document.getElementById('feedback-false'),
+  undefined: document.getElementById('feedback-undefined'),
+}
+// ----- Real 100vh ----- //
 function updateRealVH() {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
@@ -8,7 +28,7 @@ updateRealVH();
 window.addEventListener('resize', updateRealVH);
 
 
-// Share button
+// ----- Share button ----- //
 const shareButton = document.getElementById('share');
 if (navigator.share) shareButton.parentElement.style.display = 'block';
 shareButton.addEventListener('click', event => {  
@@ -24,3 +44,40 @@ shareButton.addEventListener('click', event => {
     // fallback
   }
 });
+
+// ----- Router ----- //
+const router = new Router({
+  mode: 'hash',
+  page404: (path) => {
+    console.log('"/' + path + '" Page not found');
+    console.log('Navigating to /');
+    
+    router.navigateTo('/');
+  }
+});
+
+router.add('/', () => {
+
+});
+
+router.add('/problem/(:num)', displayProblem);
+
+router.add('about', () => {
+  console.log('About Page');
+});
+
+router.addUriListener();
+
+function displayProblem(problemId) {
+  currentProblemId = problemId;
+  answerInputElem.textContent = progress.problems.lastAcceptedAnswer[currentProblemId] || '';
+  problemTitleElem.textContent = problems[currentProblemId].title;
+  problemStatementElem.textContent = problems[currentProblemId].statement;
+  feedbackElem.true.style.display = 'none';
+  feedbackElem.false.style.display = 'none';
+  feedbackElem.undefined.style.display = 'none';
+  let elem = document.querySelector('.nav--problems > .active').classList.remove('active');
+  document.querySelector(`.nav--problems > [data-id='${currentProblemId}']`).classList.add('active');
+}
+
+// TODO: Fill problem list
